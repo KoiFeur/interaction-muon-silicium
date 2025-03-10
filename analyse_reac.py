11e754eae2110c56a33f90d203f3902f0c9e0b56
robynouh <robinsonbruge@gmail.com>
2025-03-10 12:22:55 +0100

    nb de sous prod total et début energie

--- analyse_reac.py
+++ analyse_reac.py
@@ -1,9 +1,8 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def reactiontype(reactions: list) -> list: 
-    "(used to determined what's the reaction's type)"
+    "Used to determined what's the reaction's type."
     nb_el = 0
     nb_inel = 0
     nb_abs = 0
@@ -15,6 +14,15 @@ def reactiontype(reactions: list) -> list:
         elif reaction.reaction_type == "Absorption":
             nb_abs += 1
     return [nb_el, nb_inel, nb_abs]
+    
+    
+def nb_sous_prod_tot(reactions: list) -> int:
+    nb_sous_prod_tot = 0
+    for reaction in reactions:
+        nb_sous_prod_tot += reaction.nb_sous_reactifs
+    return nb_sous_prod_tot
+        
+    
 
 
             
--- main.py
+++ main.py
@@ -15,7 +15,29 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print(bcolors.FAIL + "Couldn't find the file, you may had misspelled it." + bcolors.ENDC)
         print(bcolors.FAIL + "python3 main.py [file to analyse]\n" + bcolors.ENDC)
-    #
-    # zdhrvb = analyse_reac.reactiontype(reactions)
-    # print(zdhrvb)
+        
+    
+    
+    #Dans cette section, on récupère et print le nbr de réactions el, inel et abs.
+    nb_type_reac = analyse_reac.reactiontype(reactions)
+    nb_el = nb_type_reac[0]
+    nb_inel = nb_type_reac[1]
+    nb_abs = nb_type_reac[2]
+    print("Il y a", nb_el, "réactions/chocs élastiques.")
+    print("Il y a", nb_inel, "réactions/chocs inélastiques.")
+    print("Il y a", nb_abs, "absorptions.")
+    
+    #Dans cette section, on cherche à afficher l'énergie de chaque réac sur un histogramme
+    #analyse_reac.energies(reactions)
+    
+    
+    
+    
+    
+    #Dans cette section, on cherche à déterminer le nombre de sous-produit pour tout le fichier
+    nb_sous_prod_tot = analyse_reac.nb_sous_prod_tot(reactions)
+    print("Il y a au total ", nb_sous_prod_tot, "sous-produits dans ce fichiers.")
+    
 
+    
+    
