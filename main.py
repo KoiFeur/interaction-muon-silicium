fd4f69cc46e4410ffefd53c8c823d1ddd99d7e61
n <koifeur.github@gmail.com>
2025-03-10 10:25:50 +0100

    on prépare la séance

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ import analyse_reac
 
 if __name__ == "__main__":
     try:
-        reactions = parser.parser("testing_limit.txt")
+        reactions = parser.parser("secondaries_500MeV.txt")
     except IndexError:
         print(bcolors.FAIL + "\nPlease put the name of the file here" + bcolors.ENDC)
         print(bcolors.FAIL + "python3 main.py [file to analyse]\n" + bcolors.ENDC)
     except FileNotFoundError:
         print(bcolors.FAIL + "Couldn't find the file, you may had misspelled it." + bcolors.ENDC)
         print(bcolors.FAIL + "python3 main.py [file to analyse]\n" + bcolors.ENDC)
-
-
-    zdhrvb = analyse_reac.reactiontype(reactions)
-    print(zdhrvb)
+    #
+    # zdhrvb = analyse_reac.reactiontype(reactions)
+    # print(zdhrvb)
 
