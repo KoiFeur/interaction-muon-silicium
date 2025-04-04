79f8393f5b3a2c7b9ee19c5bf7836843b69b3c32
n <koifeur.github@gmail.com>
2025-04-04 17:11:21 +0200

    changements de variables en anglais + optimisation du parser + ajouts des nouveaux fichiers

--- main.py
+++ main.py
@@ -27,18 +27,12 @@ Rappel des différents objectifs :
 
 
 
-if __name__ == "__main__":
-    try:
-        reactions = parser.parser("secondaries_500MeV.txt")
-    except IndexError:
-        print(bcolors.FAIL + "\nPlease put the name of the file here" + bcolors.ENDC)
-        print(bcolors.FAIL + "python3 main.py [file to analyse]\n" + bcolors.ENDC)
-    except FileNotFoundError:
-        print(bcolors.FAIL + "Couldn't find the file, you may had misspelled it." + bcolors.ENDC)
-        print(bcolors.FAIL + "python3 main.py [file to analyse]\n" + bcolors.ENDC)
-        
-    
-    
+def main(file):
+    reactions = parser.parser(file)
+    if type(reactions) == type(1):
+        print(f"{bcolors.FAIL}Error : reactions returned 1 exit status{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}Please read the error above or Debug the file{bcolors.ENDC}")
+
                                                                         #Dans cette section, on récupère et print le nbr de réactions el, inel et abs.
     nb_type_reac, dic_el, dic_inel, dic_abs = analyse_reac.reactiontype(reactions)
     nb_el = nb_type_reac[0]
@@ -103,126 +97,134 @@ if __name__ == "__main__":
     plt.title("Type et quantité de chaque sous-produit")
     plt.xticks(rotation=90)
     plt.show()
-    """
-    for key, value in new_dic.items():
-        if 
-    """
-
-
-
-
-    energie=analyse_reac.lvl_energie(reactions)
-
     
-   
-   
+
+
+    energy=analyse_reac.lvl_energy(reactions)
+
+
+    #j'ai juré va falloir tout changer ça là ya du rouge partout chez moi ça casse la tête
+    #+ ya explicitement marqué dans le truc qu'il y a obligation de ne pas utiliser du hard coding
+    """
     energy_rangesNe = [(0, 50), (50, 100), (100, 150), (150, 200), (200, 250), (250, 300), (300, 350), (350, 400), (400, 450), (450, 500)]
     nb_srNe=[i for i in range(len(energy_rangesNe))]
     barresNe = [0] * len(energy_rangesNe)
-    for i in range(len(energie["neutron"])):
+    for i in range(len(energy["neutron"])):
        for idx, (low, high) in enumerate(energy_rangesNe):
-           if low <= energie["neutron"][i] <= high:
+           if low <= energy["neutron"][i] <= high:
                barresNe[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesNe]    
     plt.bar(range(len(energy_rangesNe)),barresNe,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits neutron")
+    plt.title("energys des sous produits neutron")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits neutron est : {max(energie['neutron'])}")
-    print(f"Le minimum d'énergie des sous produits neutron est : {min(energie['neutron'])}")  
+    print(f"Le maximum d'énergie des sous produits neutron est : {max(energy['neutron'])}")
+    print(f"Le minimum d'énergie des sous produits neutron est : {min(energy['neutron'])}")  
 
 
     energy_rangesGam = [(0, 2), (2, 4), (4, 6), (6, 8), (8, 10), (10, 12), (12, 14), (14, 16), (16, 18)]
     nb_srGam=[i for i in range(len(energy_rangesGam))]
     barresGam = [0] * len(energy_rangesGam)
-    for i in range(len(energie["gamma"])):
+    for i in range(len(energy["gamma"])):
         for idx, (low, high) in enumerate(energy_rangesGam):
-            if low <= energie["gamma"][i] <= high:
+            if low <= energy["gamma"][i] <= high:
                 barresGam[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesGam]    
     plt.bar(range(len(energy_rangesGam)),barresGam,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits gamma")
+    plt.title("energys des sous produits gamma")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits gamma est : {max(energie['gamma'])}")
-    print(f"Le minimum d'énergie des sous produits gamma est : {min(energie['gamma'])}")    
+    print(f"Le maximum d'énergie des sous produits gamma est : {max(energy['gamma'])}")
+    print(f"Le minimum d'énergie des sous produits gamma est : {min(energy['gamma'])}")    
     
     
     energy_rangesPr = [(0, 50), (50, 100), (100, 150), (150, 200), (200, 250), (250, 300), (300, 350), (350, 400), (400, 450), (450, 500)]
     nb_srPr=[i for i in range(len(energy_rangesPr))]
     barresPr = [0] * len(energy_rangesPr)
-    for i in range(len(energie["proton"])):
+    for i in range(len(energy["proton"])):
        for idx, (low, high) in enumerate(energy_rangesPr):
-           if low <= energie["proton"][i] <= high:
+           if low <= energy["proton"][i] <= high:
                barresPr[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesPr]    
     plt.bar(range(len(energy_rangesPr)),barresPr,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits proton")
+    plt.title("energys des sous produits proton")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits proton est : {max(energie['proton'])}")
-    print(f"Le minimum d'énergie des sous produits proton est : {min(energie['proton'])}")  
+    print(f"Le maximum d'énergie des sous produits proton est : {max(energy['proton'])}")
+    print(f"Le minimum d'énergie des sous produits proton est : {min(energy['proton'])}")  
     
     
     energy_rangesSi = [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (6, 7), (7, 8), (8, 9), (9, 10)]
     nb_srSi=[i for i in range(len(energy_rangesSi))]
     barresSi = [0] * len(energy_rangesSi)
-    for i in range(len(energie["Si28"])):
+    for i in range(len(energy["Si28"])):
         for idx, (low, high) in enumerate(energy_rangesSi):
-            if low <= energie["Si28"][i] <= high:
+            if low <= energy["Si28"][i] <= high:
                 barresSi[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesSi]    
     plt.bar(range(len(energy_rangesSi)),barresSi,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits Si28")
+    plt.title("energys des sous produits Si28")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits Si28 est : {max(energie['Si28'])}")
-    print(f"Le minimum d'énergie des sous produits Si28 est : {min(energie['Si28'])}")  
+    print(f"Le maximum d'énergie des sous produits Si28 est : {max(energy['Si28'])}")
+    print(f"Le minimum d'énergie des sous produits Si28 est : {min(energy['Si28'])}")  
         
 
     energy_rangesAl = [(0, 10), (10, 20), (20, 30), (30, 40), (40, 50), (50, 60), (60, 70)]
     nb_srAl=[i for i in range(len(energy_rangesAl))]
     barresAl = [0] * len(energy_rangesAl)
-    for i in range(len(energie["alpha"])):
+    for i in range(len(energy["alpha"])):
         for idx, (low, high) in enumerate(energy_rangesAl):
-            if low <= energie["alpha"][i] <= high:
+            if low <= energy["alpha"][i] <= high:
                 barresAl[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesAl]    
     plt.bar(range(len(energy_rangesAl)),barresAl,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits alpha")
+    plt.title("energys des sous produits alpha")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits alpha est : {max(energie['alpha'])}")
-    print(f"Le minimum d'énergie des sous produits alpha est : {min(energie['alpha'])}")  
+    print(f"Le maximum d'énergie des sous produits alpha est : {max(energy['alpha'])}")
+    print(f"Le minimum d'énergie des sous produits alpha est : {min(energy['alpha'])}")  
        
 
     energy_rangesDe = [(0, 10), (10, 20), (20, 30), (30, 40), (40, 50), (50, 60)]
     nb_srDe=[i for i in range(len(energy_rangesDe))]
     barresDe = [0] * len(energy_rangesDe)
-    for i in range(len(energie["deuteron"])):
+    for i in range(len(energy["deuteron"])):
         for idx, (low, high) in enumerate(energy_rangesDe):
-            if low <= energie["deuteron"][i] <= high:
+            if low <= energy["deuteron"][i] <= high:
                 barresDe[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesDe]    
     plt.bar(range(len(energy_rangesDe)),barresDe,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits deuteron")
+    plt.title("energys des sous produits deuteron")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits deuteron est : {max(energie['deuteron'])}")
-    print(f"Le minimum d'énergie des sous produits deuteron est : {min(energie['deuteron'])}")  
+    print(f"Le maximum d'énergie des sous produits deuteron est : {max(energy['deuteron'])}")
+    print(f"Le minimum d'énergie des sous produits deuteron est : {min(energy['deuteron'])}")  
     
     
     energy_rangesAl27 = [(0, 2), (2, 4), (4, 6), (6, 8), (8, 10), (10, 12), (12, 14)]
     nb_srAl27=[i for i in range(len(energy_rangesAl27))]
     barresAl27 = [0] * len(energy_rangesAl27)
-    for i in range(len(energie["Al27"])):
+    for i in range(len(energy["Al27"])):
         for idx, (low, high) in enumerate(energy_rangesAl27):
-            if low <= energie["Al27"][i] <= high:
+            if low <= energy["Al27"][i] <= high:
                 barresAl27[idx] += 1
     name=[f"{low}-{high}eV" for  low, high in energy_rangesAl27]    
     plt.bar(range(len(energy_rangesAl27)),barresAl27,tick_label=name)
     plt.xticks(rotation=90)
-    plt.title("Energies des sous produits Al27")
+    plt.title("energys des sous produits Al27")
     plt.show()
-    print(f"Le maximum d'énergie des sous produits Al27 est : {max(energie['Al27'])}")
-    print(f"Le minimum d'énergie des sous produits Al27 est : {min(energie['Al27'])}") 
+    print(f"Le maximum d'énergie des sous produits Al27 est : {max(energy['Al27'])}")
+    print(f"Le minimum d'énergie des sous produits Al27 est : {min(energy['Al27'])}") 
+    """
+
+
+
+
+
+if __name__ == "__main__":
+    liste = np.array(["secondaries_1GeV.txt", "secondaries_1MeV.txt", "secondaries_500MeV.txt"])
+    liste = ["testing_limit.txt"]
+    for i in liste:
+        print(f"Executing file {i}")
+        main(i)
--- parser.py
+++ parser.py
@@ -30,16 +30,16 @@ class Sub_product:
     name : str
         sub product name
 
-    energie : float
+    energy : float
         sub product energy
 
     vecteur : Vecteur
         directionnal vector of the particule
 
     """
-    def __init__(self, name: str, energie: float, vec: Vecteur):
+    def __init__(self, name: str, energy: float, vec: Vecteur):
         self.name: str = name
-        self.energie: float = energie
+        self.energy: float = energy
         self.vecteur: Vecteur = vec
 
 class Reaction: 
@@ -48,84 +48,111 @@ class Reaction:
 
     Parameters:
 
-    reaction_equation: list[str]
+    eq_reac: list[str]
         List of each term that compose a reaction ["neutron", "+", "Si28", "-->", .. ]
     
     vecteur: Vecteur
         in Vector
 
-    energie: float
-        energie of the neutron
+    energy: float
+        energy of the neutron
 
-    sous_reactions : ndarray[Sub_product]
+    sub_products : ndarray[Sub_product]
         list of the reaction Sub_product
 
     nb_sous_reactifs : int
         sub product number 
     """
-    def __get_reac(self, reaction_equation: list) -> list: 
-        return [i for i in reaction_equation[:reaction_equation.index("-->")] if (i != "neutronInelastic" and i!="hadElastic" and i!= "+")]
+
+
+    def __get_reac(self, eq_reac: list) -> list: 
+        return [i for i in eq_reac[1:eq_reac.index('-->')] if i != "+"]
     
     def __get_subproducts_name(self) -> list:
-        return [i.name for i in self.sous_reactions]
+        return [i.name for i in self.sub_products]
 
 
-    def __get_reaction_type(self, reaction_equation: list) -> str:
-        if reaction_equation[0] == "hadElastic":
+    def __get_reaction_type(self, eq_reac: list) -> str:
+        if eq_reac[0] == "hadElastic":
             return "Elastique"
         else:
             #aloo
-            reac = self.__get_reac(reaction_equation)
+            reac = self.__get_reac(eq_reac)
             sub_product = self.__get_subproducts_name()
             #print(product, self.__get_subproducts_name(), "gamma" in self.__get_subproducts_name())
+            #print(reac, set(sub_product))
             if reac[0] in sub_product and reac[1] in sub_product and len(set(sub_product)) == 3:
-                #print(reac, set(sub_product))
-                #print("oui")
+                                #print("oui")
                 return "Inelastique"
+            """
+            if reac[0] in sub_product and reac[1] in sub_product and len(set(sub_product)) == 2:
+                print(reac, sub_product)
+                print(self.energy, self.reac_number, self.num_line)
+                for i in self.sub_products:
+                    print(i.name, i.energy)
+"""
+            #print(reac, sub_product)
             return "Absorption"
 
-
-    def __init__(self, energie : float, vec: Vecteur, sous_reactions: list, reaction_equation: list):
-        self.reaction_equation: list[str] = reaction_equation
+    def __init__(self, energy : float, vec: Vecteur, sub_products: list, eq_reac: list, reac_number: int, num_line: int):
+        self.reac_number: int = reac_number
+        self.num_line: int = num_line
+        self.eq_reac: list[str] = eq_reac
         self.vecteur: Vecteur = vec
-        self.energie: float = energie
-        self.sous_reactions: np.ndarray = np.array(sous_reactions)
-        self.reaction_type: str = self.__get_reaction_type(self.reaction_equation)
-        self.nb_sous_reactifs: int = len(self.sous_reactions)
+        self.energy: float = energy
+        self.sub_products: np.ndarray = np.array(sub_products)
+        self.reaction_type: str = self.__get_reaction_type(self.eq_reac)
+        self.nb_sous_reactifs: int = len(self.sub_products)
+        
+
+
 
 def parser(file: str) -> np.ndarray:
     """parse things"""
 
     print("\nReading file\n")
     liste=[""] #else there is out of range error due tu the liste[-1]+=line line
-    with open(file, "r") as dft:
-        for line in tqdm(dft): #read file
-            if "[" in line:
-                line = line.split("[")[0] + line.split("]")[1]
-            if "---" in line:
-                liste.append(line) #add each reaction line
-            else:
-                liste[-1]+=line        
+    try:
+        data = open(file, "r").read().split("\n")
+    except:
+        print("can't open the file")
+        return 1
+
+    for id in tqdm(range(len(data))): #read file
+        line = data[id] + "\n" #weird but the progress bar
+        if "[" in line:
+            line = line.split("[")[0] + line.split("]")[1]
+        if "---" in line:
+            liste.append(line[:-2] + f" {id + 1}\n") #add each reaction line
+        else:
+            liste[-1]+=line        
         #print("\nClean up : \n")
         #liste = [[[a for a in v.split(" ") if a != ""] for v in i.split("\n") if v != ""] for i in tqdm(liste)][1:] #clean up a little bit. 
                                                                                                                     #The [1:] delete the element added for the code to not crash
         #it's not necessary
 
-    print("\n\nObject list creation : \n")
-
+    print("\nObject list creation :")
+    
     reactions = []
-    for i in tqdm(liste[1:]):
-        v = [[a for a in j.split(" ") if a != ""] for j in i.split("\n") if j != ""]
+    for id in tqdm(range(1, len(liste))):
+        i = liste[id] # I know this is weird and useless but it's like that to keep the beautiful progress bar
+        v = [[a for a in j.split(" ") if a != ""] for j in i.split("\n") if j != ""] #split
 
-        energie = float(v[1][3])
+        energy = float(v[1][3])
         in_vec = Vecteur(v[1][0], v[1][1], v[1][2])
         
-        sous_reactions = []
-        for i in v[2:]:
-            sous_reactions.append(Sub_product(i[0], float(i[1]), Vecteur(i[2], i[3], i[4])))
-        reactions_equation = v[0][2:]
+        sub_products = []
+        eq_reac = v[0][2:]
+        num_line = int(eq_reac[-1])
+        eq_reac = eq_reac[:-1]
+        try:
+            for i in v[2:]:
+                sub_products.append(Sub_product(i[0], float(i[1]), Vecteur(i[2], i[3], i[4])))
+        except:
+            print(num_line)
         
-        reactions.append(Reaction(energie, in_vec, sous_reactions, reactions_equation))
+        
+        reactions.append(Reaction(energy, in_vec, sub_products, eq_reac, id, num_line))
     print("\n")
     #return reactions
     return np.array(reactions)
